dbc68b05a6666f4f0fc9579c9f596626b6d81920
Matias Laime <matulai1221@gmail.com>
2025-06-09 00:32:58 -0300

    Add: header and footer on app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer, Header } from "@app/layout"
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Footer, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
