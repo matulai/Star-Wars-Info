aff7bafc0b2bde57cec6eee95200797b719bdcaf
Matias Laime <matulai1221@gmail.com>
2025-06-19 00:26:48 -0300

    Add: routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { StarWarsService } from '@app/shared'
+import { StarWarsService } from '@app/shared/services'
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,42 @@
+import { Home, Films, Category, NotFound } from '@app/features'
 import { Routes } from '@angular/router';
-import { Home } from '@app/features'
 
 export const routes: Routes = [
   {
     path: '',
     component: Home,
+  },
+  {
+    path: 'films',
+    component: Films,
+  },
+  {
+    path: 'people',
+    component: Category,
+    data: { title: 'people' }
+  },
+  {
+    path: 'planets',
+    component: Category,
+    data: { title: 'planets' }
+  },
+    {
+    path: 'species',
+    component: Category,
+    data: { title: 'species' }
+  },
+  {
+    path: 'vehicles',
+    component: Category,
+    data: { title: 'vehicles' }
+  },
+  {
+    path: 'starships',
+    component: Category,
+    data: { title: 'starships' }
+  },
+  {
+    path: '**',
+    component: NotFound,
   }
 ];
