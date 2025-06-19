e835396fe9b2fd5a5224cac81bda06a9968065c4
Matias Laime <matulai1221@gmail.com>
2025-06-19 00:29:34 -0300

    Refactor: add name for equals to the other types

--- src/app/shared/model/film.ts
+++ src/app/shared/model/film.ts
@@ -1,7 +1,10 @@
-export interface Film {
-  title: string,
-  episode_id: number,
-  opening_crawl: string,
-  release_date: Date,
-  url: string,
+interface Film {
+  name: string;
+  title: string;
+  episode_id: number;
+  opening_crawl: string;
+  release_date: Date;
+  url: string;
 }
+
+export default Film;
