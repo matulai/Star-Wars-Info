5506765f68034f165734c20bdc41942554686b30
Matias Laime <matulai1221@gmail.com>
2025-06-21 21:53:10 -0300

    Refactor: other changes for paginations

--- src/app/features/category/category.ts
+++ src/app/features/category/category.ts
@@ -1,25 +1,41 @@
-import { Component, inject, Signal } from '@angular/core';
+import { Component, inject, Signal, signal } from '@angular/core';
+import { GenericModel, PaginatedElement } from "@app/shared/model";
+import { CardList, Pagination } from "@app/shared/components";
 import { StarWarsService } from '@app/shared/services'
 import { ActivatedRoute } from '@angular/router';
-import { GenericModel } from "@app/shared/model";
-import { CardList } from "@app/shared/components";
-import { toSignal } from '@angular/core/rxjs-interop'
 
 @Component({
   selector: 'app-category',
-  imports: [CardList],
+  imports: [CardList, Pagination],
   templateUrl: './category.html',
   styleUrl: './category.scss'
 })
 export class Category {
   pageTitle: string = '';
   service = inject(StarWarsService);
-  genericModels!: Signal<GenericModel[] | undefined>;
+  paginatedList!: Signal<PaginatedElement | undefined>;
+
 
   constructor(private route: ActivatedRoute) {
     this.route.data.subscribe(data => {
       this.pageTitle = data['title'];
-      this.genericModels = toSignal(this.service.getAll(this.pageTitle));
+      this.service.getAll(this.pageTitle).subscribe(list => {
+        this.paginatedList = signal(this.madePaginationFromList(list as GenericModel[], 10));
+      });
     });
+  }
+
+  madePaginationFromList(list: GenericModel[], size: number): PaginatedElement {
+    let pages = list.length / size;
+    return {
+      list: list,
+      pagination:{
+        actualPage: 1,
+        hasNext: pages > 1,
+        hasPrev: false,
+        totalPages: pages,
+        size: size
+      }
+    }
   }
 }
