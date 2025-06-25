a01290f42d5f0708c35a468789dd851e70effd83
Matias Laime <matulai1221@gmail.com>
2025-06-24 23:45:53 -0300

    Add: calculate actual page list to show

--- src/app/features/category/category.ts
+++ src/app/features/category/category.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Signal, signal } from '@angular/core';
+import { Component, inject, Signal, signal, computed, WritableSignal } from '@angular/core';
 import { GenericModel, PaginatedElement } from "@app/shared/model";
 import { CardList, Pagination } from "@app/shared/components";
 import { StarWarsService } from '@app/shared/services'
@@ -13,20 +13,25 @@ import { ActivatedRoute } from '@angular/router';
 export class Category {
   pageTitle: string = '';
   service = inject(StarWarsService);
-  paginatedList!: Signal<PaginatedElement | undefined>;
+  paginatedList: WritableSignal<PaginatedElement | undefined> = signal<PaginatedElement | undefined>(undefined);
+  actualListPaginate: Signal<GenericModel[]> = computed(() => {
+    const paginated = this.paginatedList();
+    console.log(paginated);
+    return paginated ? this.madeListFromPagination(paginated) : [];
+  });
 
 
   constructor(private route: ActivatedRoute) {
     this.route.data.subscribe(data => {
       this.pageTitle = data['title'];
       this.service.getAll(this.pageTitle).subscribe(list => {
-        this.paginatedList = signal(this.madePaginationFromList(list as GenericModel[], 10));
+        this.paginatedList.set(this.madePaginationFromList(list as GenericModel[], 10))
       });
     });
   }
 
   madePaginationFromList(list: GenericModel[], size: number): PaginatedElement {
-    let pages = list.length / size;
+    let pages = Math.floor(list.length / size);
     return {
       list: list,
       pagination:{
@@ -37,5 +42,12 @@ export class Category {
         size: size
       }
     }
+  }
+
+  madeListFromPagination(paginatedElement: PaginatedElement): GenericModel[] {
+    let list = paginatedElement.list;
+    let fromIndex = ( (paginatedElement.pagination.actualPage - 1) * paginatedElement.pagination.size );
+    let toIndex = (paginatedElement.pagination.actualPage * paginatedElement.pagination.size);
+    return list.slice(fromIndex, toIndex);
   }
 }
