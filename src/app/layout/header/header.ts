264243ed6860817fa4703960162ef2452899be6d
Matias Laime <matulai1221@gmail.com>
2025-06-25 23:51:12 -0300

    Fix: link on logo from header

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Navbar, Menu } from "@app/shared/components";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [Navbar, Menu],
+  imports: [Navbar, Menu, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
